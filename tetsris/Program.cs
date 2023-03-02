b7a51fa32c396284057daf1b20550a32cbe97b6f
kost <solodkovkm1997@gmail.com>
2023-03-03 00:13:15 +0300

    git init

---
+++ tetsris/Program.cs
@@ -0,0 +1,17 @@
+﻿using EntityesClass;
+using Background;
+
+namespace Tetris
+{
+    public class Game
+    {
+        static void Main(string[] args)
+        {
+            int[,] EmptyFone = new GameFone().matrix;
+            GameFone fone = new GameFone();
+            fone.PrintFone(EmptyFone);
+            Entity entity = new Entity().CreateEntity();
+            Console.WriteLine($"{entity.type} {entity.rotation} {entity.x} {entity.y}");
+        }
+    }
+}
